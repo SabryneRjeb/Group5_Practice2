0618c414fbe0cce72a3df9b9030ddb317455e342
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-01-17 16:53:20 -0500

    Second commit

--- src/Day3/Practice1.java
+++ src/Day3/Practice1.java
@@ -1,2 +1,8 @@
-package Day3;public class Practice1 {
+package Day3;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("Hallo");
+        System.out.println("Hello world");
+    }
 }
